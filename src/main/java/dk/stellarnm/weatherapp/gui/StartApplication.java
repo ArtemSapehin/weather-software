fdbfd07eb14c4e75aa38e46acc3567a01cd5b095
ArtemSapehin <minethegame4@gmail.com>
2026-05-09 01:47:43 +0200

    Weather App Design

--- src/main/java/dk/stellarnm/weatherapp/gui/StartApplication.java
+++ src/main/java/dk/stellarnm/weatherapp/gui/StartApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,8 +13,9 @@ public class StartApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Weather Software");
+        stage.setTitle("Weather Application");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 }
